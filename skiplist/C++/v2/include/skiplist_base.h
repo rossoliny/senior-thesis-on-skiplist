a0bafa6217283870a77e5de40953d276c02439fd
Rossolinyisanautist <dzhumabaev_i@auca.kg>
2021-04-07 15:38:01 +0600

    fix bugs. All tests are passed

--- skiplist/C++/v2/include/skiplist_base.h
+++ skiplist/C++/v2/include/skiplist_base.h
@@ -215,9 +215,7 @@ namespace isa
 		template<typename... Args>
 		insert_return_t append_or_insert(Args&&... args)
 		{
-			pair_type data(std::forward<Args>(args)...);
-
-			return append_or_insert(std::move(data));
+			return append_or_insert(std::forward<Args>(args)...);
 		}
 
 		template<typename... Args>
