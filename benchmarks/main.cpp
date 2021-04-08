124aa8a7d93f8d411d9e5a6eb39507ce776cf519
Rossolinyisanautist <dzhumabaev_i@auca.kg>
2021-04-09 01:51:04 +0600

    simple bench

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -3,10 +3,13 @@
 #include <map>
 #include <string>
 #include <chrono>
+#include <thread>
 
 
 int main()
 {
+	using namespace std::literals::chrono_literals;
+
 	isa::map<std::string, int> m1;
 	std::map<std::string, int> m2;
 
@@ -22,8 +25,8 @@ int main()
 	auto end2 = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<float> elapsed2 = end2 - start2;
 
-	std::cout << "isa::map::insert:\t" << (elapsed.count()) << " sec" << std::endl;
-	std::cout << "std::map::insert:\t" << (elapsed2.count()) << " sec" << std::endl;
+	std::cout << "isa::map::insert:\t" << (elapsed.count() * 1000) << " ms" << std::endl;
+	std::cout << "std::map::insert:\t" << (elapsed2.count() * 1000) << " ms" << std::endl;
 
 	return 0;
 }
