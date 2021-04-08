6ebd4d01ab7c0cb015bc73a7e90f609eeebce2fd
Rossolinyisanautist <dzhumabaev_i@auca.kg>
2021-04-09 01:40:09 +0600

    simple bench

---
+++ benchmarks/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <smap>
+#include <map>
+#include <string>
+#include <chrono>
+
+
+int main()
+{
+	isa::map<std::string, int> m1;
+	std::map<std::string, int> m2;
+
+	auto p = std::make_pair("short string", 10);
+
+	auto start = std::chrono::high_resolution_clock::now();
+	m1.insert(p);
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<float> elapsed = end - start;
+
+	auto start2 = std::chrono::high_resolution_clock::now();
+	m2.insert(p);
+	auto end2 = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<float> elapsed2 = end2 - start2;
+
+	std::cout << "isa::map::insert:\t" << (elapsed.count()) << " sec" << std::endl;
+	std::cout << "std::map::insert:\t" << (elapsed2.count()) << " sec" << std::endl;
+
+	return 0;
+}
